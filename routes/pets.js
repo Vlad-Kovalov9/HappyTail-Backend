8882480a8a87680210018a227c9ffa483af00b01
Vlad <nowornever969@gmail.com>
2025-09-14 16:20:44 +0300

    Fix pets sort

--- routes/pets.js
+++ routes/pets.js
@@ -4,8 +4,13 @@ import { Pet } from "../models/Pet.js";
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-  const pets = await Pet.find();
-  res.json(pets);
+  try {
+    const pets = await Pet.find().sort({ _id: 1 });
+    res.json(pets);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Server error" });
+  }
 });
 
 export default router;
